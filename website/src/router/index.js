bcbe848f585bee135ff6fa2a4b6c32960232b424
xuheyuan <heyuanxu_neu@163.com>
2026-06-24 16:36:45 +0800

    更新工具页面与新增OxdnaViewerPage

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -3,12 +3,14 @@ import HomePage from '../views/HomePage.vue'
 import ArchitecturePage from '../views/ArchitecturePage.vue'
 import ApplicationsPage from '../views/ApplicationsPage.vue'
 import ToolsPage from '../views/ToolsPage.vue'
+import OxdnaViewerPage from '../views/OxdnaViewerPage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/architecture', name: 'Architecture', component: ArchitecturePage },
   { path: '/applications', name: 'Applications', component: ApplicationsPage },
   { path: '/tools', name: 'Tools', component: ToolsPage },
+  { path: '/tools/oxdna-viewer', name: 'OxdnaViewer', component: OxdnaViewerPage },
 ]
 
 const router = createRouter({
